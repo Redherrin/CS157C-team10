a14b1772b0e2b6a7fb53e72a8c64067d1d4eba8b
brchao409 <brchao409@gmail.com>
2023-05-10 21:01:19 -0700

    Added username display on navbar when logged in

--- ui/src/app/components/app.component.ts
+++ ui/src/app/components/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import jwt_decode from 'jwt-decode';
 
 import { UserService } from '../services/user.service';
 import { TokenStorageService } from '../services/token-storage.service';
@@ -10,8 +11,16 @@ import { TokenStorageService } from '../services/token-storage.service';
 })
 export class AppComponent {
   title = 'All Your Rock';
+  token: string | null;
+  username = '';
 
   constructor(
-    public tokenStorageService: TokenStorageService
-  ) {}
+    public tokenStorageService: TokenStorageService,
+  ) {
+    this.token = this.tokenStorageService.getToken();
+    if(this.token !== null){
+      const decodedToken = jwt_decode(this.token) as { [key: string]: any };
+      this.username = decodedToken['username'];
+    }
+  }
 }
